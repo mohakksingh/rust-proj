b90f7aff902151a6cc67ede7a4145a2aca591b2b
quanta2k <72522871+quanta2k@users.noreply.github.com>
2024-04-30 00:30:29 +0530

    functions in rust

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,10 @@
 fn main(){
-    //arrays ,maps,strings
-    let sentence=String::from("my name is mohak");
-    let first_word=get_first_word(sentence);
-
-    let n=1000;
-    for i in 0..n{
-        println!("Hello world,! {}",i);
-    }
-
-    println!("First word is : {}",first_word);
-
+    let a=10;
+    let b=10;
+    let sum = do_sum(a, b);
+    println!("Sum of {} and {} is {} ",a,b,sum);
 }
 
-fn get_first_word(sentence:String)->String{
-    let mut ans: String =String::from("");
-    for char in sentence.chars(){
-        ans.push_str(char.to_string().as_str());
-        if char == ' ' {
-            break;
-        }
-    }
-
-    return ans;
-
+fn do_sum(a:i32,b:i32)->i32{
+    return a+b;
 }
