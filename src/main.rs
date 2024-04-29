8a22494faa139b6a18c1fe387ca8ab4e8a36a2b2
quanta2k <72522871+quanta2k@users.noreply.github.com>
2024-04-30 00:02:57 +0530

    Add variables x, y, and z and print their values in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+fn main(){
+    let x: i32 = 5;
+    let y: u32 =1111;
+    let z: f32 = 1000.001;
+
+    print!("x: {}",x);
+    print!("y: {}",y);
+    print!("z: {}",z);
 }
