62cd6e4fb5daf3662ee7c98fc794563a94f8d142
quanta2k <72522871+quanta2k@users.noreply.github.com>
2024-05-01 15:17:58 +0530

    memory management in rust

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,35 @@
+
+
 fn main(){
-    let a=10;
-    let b=10;
-    let sum = do_sum(a, b);
-    println!("Sum of {} and {} is {} ",a,b,sum);
+    stack_fn();//call the function that uses stack
+    heap_fn();//call the function that uses heap
+    update_string();//call the function that changes the size of variable at runtime
 }
 
-fn do_sum(a:i32,b:i32)->i32{
-    return a+b;
+fn stack_fn(){
+    //declare a few integers on the stack
+    let a=10;
+    let b=20;
+    let c=a+b;
+    println!("Stack function: the sum of {} and {} is {}",a,b,c);
+}
+
+fn heap_fn(){
+    //create a string which is allocated on the heap
+    let s1= String::from("Hello");
+    let s2=String::from("World");
+    let combined= format!("{} {}",s1,s2);
+    println!("Heap function: Combined string is : {}",combined);
+
+}
+
+fn update_string(){
+    //start with a base string on a heap
+    let mut s=String::from("Initial string");
+    println!("Before update: {}",s);
+    println!("Capacity: {},lenght: {},pointer: {:p}",s.capacity(),s.len(),s.as_ptr());
+
+    //append some text to the string
+    s.push_str("add some additional text");
+    println!("After update: {}",s);
 }
