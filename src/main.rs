0efa51ec01b437d7db63e3992504b24cb05edfa8
quanta2k <72522871+quanta2k@users.noreply.github.com>
2024-05-03 14:10:39 +0530

    enum and pattern matching

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
-struct NoShape;
+enum Shape{
+    Circle(f64),
+    Square(f64),
+    Rectange(f64,f64),
+}
 
-impl NoShape{
-    fn area(&self)->u32{
-        return 0;
-    }
+fn calculate_area(shape:Shape)->f64{
+    //calculate the area of the shape
+    let ans= match shape{
+        Shape::Circle(radius)=>3.14*radius*radius,
+        Shape::Rectange(width,height)=>{
+            width*height
+        },
+        Shape::Square(side)=>side*side
+    };
+    return ans;
 }
 
 fn main(){
-    let rect= NoShape;
-    print!("{:?}",rect.area());
+    let circle=Shape::Circle(5.0);
+    let square=Shape::Square(10.0);
+    let rect=Shape::Rectange(21.23, 4.4);
+    
+    let area=calculate_area(circle);
+    print!("Area of circle is {}\n",area);
 }
